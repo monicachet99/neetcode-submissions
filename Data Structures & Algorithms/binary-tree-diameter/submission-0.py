a5980133a7ff3dcfcf57735462f79610c7605ada
monica <monica.basavaraju99@gmail.com>
2026-06-23 22:07:09 -0400

    Add: binary-tree-diameter - submission-0

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.ans = 0
+
+        def depth(node):
+            if not node:
+                return 0
+
+            l = depth(node.left)
+            r = depth(node.right)
+
+            self.ans = max(self.ans, l + r)
+
+            return 1 + max(l, r)
+
+        depth(root)
+        return self.ans
